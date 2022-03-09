8adb31a0e4c93d66c951c5cfdd241987035fb2b3
Daniel Shanklin <80849763+dshanklin-501-data-solutions@users.noreply.github.com>
2022-03-09 11:54:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hello 501!!!! Node.js Sample!\n');
+});
+
+app.get('/test1', (req, res) => {
+  res.send('I am a test1\n');
 });
 
 var port = process.env.PORT||PORT;
